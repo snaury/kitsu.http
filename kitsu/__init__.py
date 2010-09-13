159dcfd1717ee5e7afd445f5cc3fca94db4799de
Alexey Borzenkov <snaury@gmail.com>
2010-09-14 01:00:43 +0400

    Don't pollute kitsu namespace with symbols

--- kitsu/__init__.py
+++ kitsu/__init__.py
@@ -1,12 +1,15 @@
 __all__ = []
 
+# The code below is based on Paste
+
 try:
     import pkg_resources
     pkg_resources.declare_namespace(__name__)
+    del pkg_resources
 except ImportError:
-    # don't prevent use of paste if pkg_resources isn't installed
     from pkgutil import extend_path
     __path__ = extend_path(__path__, __name__)
+    del extend_path
 
 try:
     import modulefinder
@@ -15,3 +18,4 @@ except ImportError:
 else:
     for p in __path__:
         modulefinder.AddPackagePath(__name__, p)
+    del modulefinder
